29ff71de322ef0c58369c1a669da4c8d3149d252
Jake Shadle <jake.shadle@embark-studios.com>
2019-04-11 11:31:37 +0200

    Add test script

---
+++ hooks/command.bat
@@ -0,0 +1,2 @@
+REM execute the powershell script in the same directory as this batch file
+powershell %~dp0command.ps1
---
+++ hooks/command.ps1
@@ -0,0 +1,5 @@
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
+
+echo "Excellent, we actually executed our amazing powershell script :windows:"
+exit 0
